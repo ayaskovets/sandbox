f59e7b4e6591887cbadf2042108e8c61ab74008d
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-10-31 11:35:37 +0300

    Add random-related problems

--- Problems/Problems21_28.hs
+++ Problems/Problems21_28.hs
@@ -1,5 +1,6 @@
 module Problems.Problems21_28 where
 
+import Data.List
 import System.Random
 
 -- Insert an element at a given position into a list
@@ -14,10 +15,24 @@ problem22 a b
     | otherwise = [a]
 
 -- Extract a given number of randomly selected elements from a list
+problem23 :: [a] -> Int -> IO [a]
+problem23 _ 0 = return []
+problem23 x n = do
+    i  <- getStdRandom (randomR (0, (length x)-1))
+    xs <- problem23 (take i x ++ drop (i+1) x) (n-1)
+    return ((x !! i):xs)
 
 -- Lotto: Draw N different random numbers from the set 1..M
+problem24 :: Int -> Int -> IO [Int]
+problem24 n m
+    | n > m     = error "Invalid arguments"
+    | otherwise = do
+        g <- getStdGen
+        return . take n . nub $ randomRs (1, m) g
 
 -- Generate a random permutation of the elements of a list
+problem25 :: [a] -> IO [a]
+problem25 x = problem23 x (length x)
 
 -- Generate the combinations of K distinct objects chosen from the N elements of a list
 
