85a255a40fa05db10dd6169c5ec6ddfc6e63897c
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-01 14:56:32 +0300

    Strategy is finished

--- behavioral/strategy.hpp
+++ behavioral/strategy.hpp
@@ -1,7 +1,35 @@
 #pragma once
 
 namespace patterns {
-}
 
-namespace patterns_cpp {
-}
+class AbstractStrategy {
+ public:
+  virtual void interface() = 0;
+};
+
+class ConcreteStrategyA : public AbstractStrategy {
+ public:
+  void interface() override {
+    // ConcreteStrategyA-specific
+  }
+};
+
+class ConcreteStrategyB : public AbstractStrategy {
+ public:
+  void interface() override {
+    // ConcreteStrategyB-specific
+  }
+};
+
+class Worker {
+ private:
+  AbstractStrategy* strategy;
+
+ public:
+  void setStrategy(AbstractStrategy* strat) { strategy = strat; }
+  void work() { strategy->interface(); }
+};
+
+}  // namespace patterns
+
+namespace patterns_cpp {}
