c97abb071702241ea40e64f9b9481a82e6ac3bb4
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-01 14:56:27 +0300

    Proxy is finished

--- structural/proxy.hpp
+++ structural/proxy.hpp
@@ -2,8 +2,34 @@
 
 namespace patterns {
 
-}
+class WorkerInterface {
+ public:
+  virtual void operate() = 0;
+};
+
+class Worker : public WorkerInterface {
+ public:
+  void operate() {
+    // ConcreteWorker-specific
+  }
+};
+
+class Proxy : public WorkerInterface {
+ protected:
+  Worker worker;
+
+ public:
+  Proxy(Worker worker) : worker(worker) {}
+
+  void operate() {
+    // Proxy-specific
+    worker.operate();
+  }
+
+  // other Proxy-specific methods
+};
+
+}  // namespace patterns
 
 namespace patterns_cpp {
-
 }
