b56c0d873887928ba2f765eff7583e6f8d014039
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-01 14:56:34 +0300

    Memento is finished

--- behavioral/memento.hpp
+++ behavioral/memento.hpp
@@ -1,7 +1,45 @@
 #pragma once
 
-namespace patterns {
-}
+#include <list>
 
-namespace patterns_cpp {
-}
+namespace patterns {
+
+class State {
+  // State implementation
+};
+
+class Memento {
+ private:
+  State* state;
+
+ public:
+  Memento(State* st) : state(st) {}
+
+  State* getState() { return state; }
+};
+
+class Originator {
+ private:
+  State* state;
+
+ public:
+  Memento* createMemento() { return new Memento(state); }
+
+  void restore(Memento* memento) { state = memento->getState(); }
+};
+
+class Caretaker {
+ private:
+  std::list<Memento*> mementos;
+  Originator* originator;
+
+ public:
+  Caretaker(Originator* orig) : originator(orig) {}
+
+  void backup() { mementos.push_back(originator->createMemento()); }
+  // Caretaker-specific
+};
+
+}  // namespace patterns
+
+namespace patterns_cpp {}
