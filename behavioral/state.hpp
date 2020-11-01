97a746bd1832933df7f550330426654c4e66bfc5
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-01 14:56:33 +0300

    State is finished

--- behavioral/state.hpp
+++ behavioral/state.hpp
@@ -1,7 +1,42 @@
 #pragma once
 
 namespace patterns {
-}
 
-namespace patterns_cpp {
-}
+class StateContext;
+
+class AbstractState {
+ protected:
+  StateContext* context;
+
+ public:
+  void setContext(StateContext* ctx) { context = ctx; }
+  virtual void interface() = 0;
+};
+
+class ConcreteStateA : public AbstractState {
+ public:
+  void interface() override {}
+};
+
+class ConcreteStateB : public AbstractState {
+ public:
+  void interface() override {}
+};
+class StateContext {
+ private:
+  AbstractState* state;
+
+ public:
+  StateContext(AbstractState* st) { transitionTo(state); }
+
+  void transitionTo(AbstractState* st) {
+    state = st;
+    state->setContext(this);
+  }
+
+  void request() { state->interface(); }
+};
+
+}  // namespace patterns
+
+namespace patterns_cpp {}
