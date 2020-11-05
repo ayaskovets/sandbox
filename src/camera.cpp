d0a8e7245db2d772968dbc412bf0c46a65ba6e07
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-05 13:08:39 +0300

    Add camera class

---
+++ src/camera.cpp
@@ -0,0 +1,55 @@
+#include "camera.hpp"
+
+#include "glm/gtc/quaternion.hpp"
+
+Camera::Camera(glm::vec3 pos, glm::vec3 front, glm::vec3 right, glm::vec3 up)
+: pos(pos)
+, front(front)
+, right(right)
+, up(ip)
+{}
+
+Camera::Camera(glm::vec3 pos)
+: Camera(pos, glm::vec3(0, 0, -1), glm::vec3(1, 0, 0), glm::vec3(0, 1, 0))
+{}
+
+Camera::Camera()
+: Camera(glm::vec3(0, 0, 0))
+{}
+
+void Camera::move(CameraMoveDirection dir, GLfloat dist)
+{
+    switch (dir)
+    {
+    case CameraMoveDirection::Forward:  pos += front * dist; break;
+    case CameraMoveDirection::Backward: pos -= front * dist; break;
+    case CameraMoveDirection::Left:     pos -= right * dist; break;
+    case CameraMoveDirection::Right:    pos += right * dist; break;
+    case CameraMoveDirection::Up:       pos += up * dist;    break;
+    case CameraMoveDirection::Down:     pos -= up * dist;    break;
+    }
+}
+
+void Camera::rotate(CameraRotationDirection dir, GLfloat angle)
+{
+    switch (dir)
+    {
+    case CameraRotationDirection::Yaw:
+        front = glm::normalize(front * glm::angleAxis(glm::radians(angle), up));
+        right = glm::normalize(glm::cross(front, up));
+        break;
+    case CameraRotationDirection::Pitch:
+        front = glm::normalize(front * glm::angleAxis(glm::radians(angle), -right));
+        right = glm::normalize(glm::cross(front, up));
+        break;
+    case CameraRotationDirection::Roll:
+        right = glm::normalize(right * glm::angleAxis(glm::radians(angle), -front));
+        up = glm::normalize(glm::cross(right, front));
+        break;
+    }
+}
+
+glm::mat4 Camera::getView() const
+{
+  return glm::lookAt(pos, pos + front, up);
+}
---
+++ src/camera.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+enum class CameraMoveDirection
+{
+    Forward,
+    Backward,
+    Left,
+    Right,
+    Up,
+    Down
+};
+
+enum class CameraRotationDirection
+{
+    Yaw,
+    Pitch,
+    Roll
+};
+
+class Camera
+{
+    glm::vec3 pos;
+
+    glm::vec3 front;
+    glm::vec3 right;
+    glm::vec3 up;
+
+public:
+    Camera(glm::vec3 Pos, glm::vec3 Front, glm::vec3 Right, glm::vec3 Up);
+    explicit Camera(glm::vec3 Pos);
+    Camera();
+
+    void move(CameraMoveDirection dir, GLfloat dist);
+    void rotate(CameraRotationDirection dir, GLfloat angle);
+
+    glm::mat4 getView() const;
+};
