ef94520d51a5eedf74fb8c37ae6ea70258a69c3c
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2020-11-05 13:08:40 +0300

    Refactor GLobject

---
+++ src/gl_util/globject.cpp
@@ -0,0 +1,25 @@
+#include "globject.hpp"
+
+namespace gl_util
+{
+
+GLobject::GLobject() noexcept
+: id(0)
+{}
+
+GLobject::GLobject(GLuint id) noexcept
+: id(id)
+{}
+
+GLobject::GLobject(GLobject&& object) noexcept
+: GLobject(object.id)
+{
+    object.id = 0;
+}
+
+GLuint GLobject::getID() const noexcept
+{
+    return id;
+}
+
+}
---
+++ src/gl_util/globject.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include "util/mixin.hpp"
+
+namespace gl_util
+{
+
+class GLobject : public util::NonCopyable
+{
+protected:
+    GLuint id;
+
+    GLobject() noexcept;
+    explicit GLobject(GLuint id) noexcept;
+    GLobject(GLobject&& object) noexcept;
+    GLobject& operator=(GLobject&& object) = delete;
+
+public:
+    GLuint getID() const noexcept;
+};
+
+}
